b4c476ae27eced9835b365bd88daa1a64b276592
ubercreep <slavadem3106@gmail.com>
2025-10-02 10:54:41 +0300

    Delete lab1 directory

--- lab1/build/my_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/mnt/c/Users/Вячеслав/Desktop/oop/lab1/build/my_tests[1]_tests.cmake")
-  include("/mnt/c/Users/Вячеслав/Desktop/oop/lab1/build/my_tests[1]_tests.cmake")
-else()
-  add_test(my_tests_NOT_BUILT my_tests_NOT_BUILT)
-endif()
--- lab1/include/func.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <string>
-
-std::string func(std::string text, int n, char old_value, char new_value);
